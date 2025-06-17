d392154a7148bb2b6cff3e32569c114c5bb2d8a6
Lafi Wafa <137789332+wafa19971997@users.noreply.github.com>
2025-06-17 11:59:01 +0100

    Update Runner.java

--- src/test/java/run/Runner.java
+++ src/test/java/run/Runner.java
@@ -9,14 +9,14 @@ import io.cucumber.junit.CucumberOptions;
 //@CucumberOptions(tags = "@DevisCasCorrecteTC3" + "" + "" + "",
 //@CucumberOptions(tags = "@Erudaxis_NavigationDevis" + "" + "" + "",
 //@CucumberOptions(tags = "@TC4" + "" + "" + "",
-@CucumberOptions(tags = "@Salles"
+@CucumberOptions(tags = "@CalculDevis"
 		+ "" + "" + "" + "",
 		// glue = cest ou je trouve les annotation cucumber (@when /@then /
 		// @before/@after ...)
 		glue = { "stepDefinition", "hooks" },
 		// features= clique droite et propriete ==> copier path a partir de src/... du
 		// Package testCasesZutomationEexercice qui se trouve dans src/test/features
-		features = "src/test/features/salles", monochrome = false, publish = true, plugin = { "pretty",
+		features = "src/test/features/devis", monochrome = false, publish = true, plugin = { "pretty",
 				"html:target/cucumber-report.html", "json:target/cucumber-report.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
 public class Runner {
